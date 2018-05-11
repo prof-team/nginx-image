e270b03a0c79a7f41b0d63c680e78ed8ff112451
Grischuk Sasha <grischuk.alexandr@gmail.com>
2018-05-11 18:30:11 +0300

    rm logrotate and supervisor

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
 FROM nginx:1.13
 
 RUN apt-get update \
-	&& apt-get install -y htop nano git logrotate supervisor
-
-ADD ./logrotate/nginx /etc/logrotate.d/nginx
+	&& apt-get install -y htop nano git
 
 ADD ./nginx.conf /etc/nginx/
 ADD ./expires.conf /etc/nginx/
@@ -13,20 +11,17 @@ ADD ./php-fpm.conf /etc/nginx/
 
 ADD ./conf.d/*.conf /etc/nginx/conf.d/
 
-ADD ./supervisord/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
-
 RUN rm /etc/nginx/conf.d/default.conf
-
-RUN rm /etc/cron.daily/logrotate
-ADD ./cron/logrotate /etc/cron.d/logrotate
-RUN crontab /etc/cron.d/logrotate
 
 RUN mkdir -p /var/www && chown -R www-data:www-data /var/www
 
 RUN apt-get clean && apt-get autoclean && apt-get autoremove -y
 RUN rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-CMD ["/usr/bin/supervisord", "-c", "/etc/supervisor/supervisord.conf"]
+COPY docker-entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["sh", "/entrypoint.sh"]
+CMD ["nginx", "-g", "daemon off;"]
 
 WORKDIR /var/www
 
---
+++ docker-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+if [ -n "$WORKER_PROCESSES" ]; then
+    sed -i "s/worker_processes.*/worker_processes $WORKER_PROCESSES;/" /etc/nginx/nginx.conf
+fi
+if [ -n "$WORKER_CONNECTIONS" ]; then
+    sed -i "s/worker_connections.*/worker_connections $WORKER_CONNECTIONS;/" /etc/nginx/nginx.conf
+fi
+if [ -n "$CLIENT_MAX_BODY_SIZE" ]; then
+    sed -i "s/client_max_body_size.*/client_max_body_size $CLIENT_MAX_BODY_SIZE;/" /etc/nginx/nginx.conf
+fi
+if [ -n "$FASTCGI_PASS" ]; then
+    sed -i "s/fastcgi_pass .*/fastcgi_pass $FASTCGI_PASS;/" /etc/nginx/php-fpm.conf
+fi
+if [ -n "$FASTCGI_CONNECT_TIMEOUT" ]; then
+    sed -i "s/fastcgi_connect_timeout.*/fastcgi_connect_timeout $FASTCGI_CONNECT_TIMEOUT;/" /etc/nginx/php-fpm.conf
+fi
+if [ -n "$FASTCGI_READ_TIMEOUT" ]; then
+    sed -i "s/fastcgi_read_timeout.*/fastcgi_read_timeout $FASTCGI_READ_TIMEOUT;/" /etc/nginx/php-fpm.conf
+fi
+if [ -n "$FASTCGI_SEND_TIMEOUT" ]; then
+    sed -i "s/fastcgi_send_timeout.*/fastcgi_send_timeout $FASTCGI_SEND_TIMEOUT;/" /etc/nginx/php-fpm.conf
+fi
+if [ -n "$GZIP_STATUS" ]; then
+    sed -i "s/gzip .*/gzip $GZIP_STATUS;/" /etc/nginx/conf.d/gzip.conf
+    sed -i "s/gzip_vary .*/gzip_vary $GZIP_STATUS;/" /etc/nginx/conf.d/gzip.conf
+fi
+
+exec "$@"
