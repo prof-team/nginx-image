f61b4440a39a262a31f8d8d8f451c589f6dd29c0
Grischuk Sasha <grischuk.alexandr@gmail.com>
2019-08-14 13:30:13 +0300

    add geoip

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -31,7 +31,7 @@ if [ -n "$GZIP_STATUS" ]; then
     sed -i "s/gzip_vary .*/gzip_vary $GZIP_STATUS;/" /etc/nginx/conf.d/gzip.conf
 fi
 
-if [ -z "$ENABLE_GEOIP_MODULE" ] || [ "$ENABLE_GEOIP_MODULE" -eq 0 ]; then
+if [ -n "$ENABLE_GEOIP_MODULE" ] && [ "$ENABLE_GEOIP_MODULE" -eq 1 ]; then
     sed -i "s/#GEOIP_MODULE.*/load_module \"modules\/ngx_http_geoip_module.so\";/" /etc/nginx/nginx.conf
 fi
 
